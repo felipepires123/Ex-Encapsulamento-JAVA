c9dbc47980e088d4c3b30301a7f655f6332d773f
Carlos F. R. Pires <cfrpires@gmail.com>
2022-10-28 18:28:55 -0300

    Fiz o exercício

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Controle c1 = new Controle();
+
+        c1.ligar();
+        c1.play();
+        c1.pause();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.ligarMudo();
+        c1.abrirMenu();
+        c1.fecharMenu();
+
+    }
+}
